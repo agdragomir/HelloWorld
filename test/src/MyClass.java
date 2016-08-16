a3a33d2796982aceecb5548a0c166d3e4485a2d8
Alina G. Dragomir <alina-gabriela.dragomir@univie.ac.at>
2016-08-16 19:35:59 +0200

    added not working Hello World

---
+++ test/src/MyClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Alina on 16.08.2016.
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
