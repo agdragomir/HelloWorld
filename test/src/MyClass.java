8c2f3c257e1208f7f288d853376e96db4bde14e2
Alina G. Dragomir <alina-gabriela.dragomir@univie.ac.at>
2016-08-16 19:40:53 +0200

    changed not working Hello Worlds__UNI

--- test/src/MyClass.java
+++ test/src/MyClass.java
@@ -4,6 +4,6 @@
 public class MyClass {
     public static void main(String[] args) {
         // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, Worlds");
+        System.out.println("Hello, Worlds__ UNI PROJEKT");
     }
 }
